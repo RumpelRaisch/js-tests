52efcaf28339803f8495360ef558bad077518b12
RumpelRaisch <kornfreak66666@netscape.net>
2018-09-29 17:03:23 +0200

    added favicon

--- tests/history-api/html.php
+++ tests/history-api/html.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <meta name="author" content="Rainer Schulz">
+    <link rel="icon" href="<?php echo HTTP_BASE; ?>favicon.ico">
 
     <title><?php echo $data['title']; ?></title>
 
